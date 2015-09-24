ba3aac5852f7ccc4135d5aeddb903e096d5d792f
adaau <ada.au.cheng@gmail.com>
2015-09-24 22:21:33 +0800

    done

--- starter-code/functions.js
+++ starter-code/functions.js
@@ -1,52 +1,86 @@
 // Question 1
-function maxOfTwoNumbers() {
-
+function maxOfTwoNumbers(x, y) {
+    if (x >= y) {
+        return x;
+    }
+    else if (y > x) {
+        return y;
+    }
+    else {
+        return null;
+    }
 }
 
 // Question 2
-function maxOfThree() {
-
+function maxOfThree(x, y, z) {
+    return Math.max(x, y, z);
 }
 
 // Question 3
-function isCharacterAVowel() {
+var vowels = ["a", "e", "i", "o", "u"];
 
+function isCharacterAVowel(x) {
+    var ind = vowels.indexOf(x);
+    if (ind >= 0) {
+        return true;
+    }
+    else {
+        return false;
+    }
 }
 
 // Question 4
-function sumArray() {
- 
+function sumArray(array) {
+    for (var i = 0, sum = 0; i < array.length; i++) {
+        sum += array[i];
+    }
+    return sum;
 }
 
-
 // Question 4
-function multiplyArray() {
-
+function multiplyArray(array) {
+    for (var i = 0, product = 1; i < array.length; i++) {
+        product *= array[i];
+    }
+    return product;
 }
 
 
 // Question 5
 var numberOfArguments = function(){
-  
+	return arguments.length;  
 }
-
-
 
 // Question 6
-var reverseString = function (){
-  
-};
-
-
-// Question 7
-function findLongestWord () {
-  
+function reverseString(string) {
+    var reverseArray = [];
+    for (var i = 1; i <= string.length; i++) {
+        reverseArray.push(string.charAt(string.length - i));   
+    }    
+        var reverseWord = reverseArray.join("");
+        return reverseWord;
 }
 
+// Question 7
+function findLongestWord(array) {
+    for (var i = 0, longest = 0; i < array.length; i++) {
+        if (array[i].length > longest) {
+            longest = array[i].length;
+            longestWord = array[i];
+        }
+    }  
+    return longestWord;
+}
 
 // Question 8
-function filterLongWords () {
-  
+var newArray = [];
+function filterLongWords(array, i) {
+    for (var x = 0; x < array.length; x++) {
+        if (array[x].length > i) {
+            newArray.push(array[x]);
+        }
+    }
+    return newArray;
 }
 
 
